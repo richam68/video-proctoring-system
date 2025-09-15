ee71c564de110ac258f4455585863fd2246ab044
richam68 <richamhshwr2@gmail.com>
2025-09-16 00:14:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Interview from './components/Interview'
+
+function App() {
+  return (
+    <div className="App" style={{ padding: 16 }}>
+      <h1>Tute Dude Project</h1>
+      <Interview />
+    </div>
+  )
+}
+
+export default App
